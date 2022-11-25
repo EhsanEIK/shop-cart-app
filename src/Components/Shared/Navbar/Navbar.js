2fcf26ff05b85a6f0d6dd7b0e6170a5c39efada7
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-25 16:26:39 +0600

    added menu items at the bottom of navbar

--- src/Components/Shared/Navbar/Navbar.js
+++ src/Components/Shared/Navbar/Navbar.js
@@ -11,6 +11,23 @@ const Navbar = () => {
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 block text-md text-gray-500 gap-5">
+                            <li>Fresh</li>
+                            <li>Today's Deals</li>
+                            <li>Mobiles</li>
+                            <li>Gift Cards</li>
+                            <li>Women Clothing</li>
+                            <li>Men Clothing</li>
+                            <li>Kids Clothing</li>
+                            <li>Health</li>
+                            <li>Pet Corner</li>
+                            <li>Books</li>
+                            <li>Beauty</li>
+                            <li>Kitchen</li>
+                            <li>Bed Room</li>
+                            <li>Sport</li>
+                            <li>Bags</li>
+                        </ul>
                     </div>
                     <Link to='/' className="btn btn-ghost normal-case text-3xl">
                         <span className='font-bold'>shop</span>
@@ -39,6 +56,23 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
+            <ul className='hidden lg:flex justify-center text-md text-gray-500 gap-5'>
+                <li>Fresh</li>
+                <li>Today's Deals</li>
+                <li>Mobiles</li>
+                <li>Gift Cards</li>
+                <li>Women Clothing</li>
+                <li>Men Clothing</li>
+                <li>Kids Clothing</li>
+                <li>Health</li>
+                <li>Pet Corner</li>
+                <li>Books</li>
+                <li>Beauty</li>
+                <li>Kitchen</li>
+                <li>Bed Room</li>
+                <li>Sport</li>
+                <li>Bags</li>
+            </ul>
         </div >
     );
 };
