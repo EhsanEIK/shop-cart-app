229b194f7b7cc8a72a07ee15e11dda5898a96362
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-25 15:44:40 +0600

    router setup has been done

---
+++ src/Routes/Routes/routes.js
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Home</div>
+    }
+])
+
+export default router;
